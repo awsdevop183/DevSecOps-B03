c767e0f4d0a3ce7c3d38b61ce106d2b1158b3d81
Madhukar Reddy <info@madhukarreddy>
2026-07-22 08:33:29 +0530

    Add subnet-2

--- subnets.tf
+++ subnets.tf
@@ -6,3 +6,13 @@ resource "aws_subnet" "git-sub-1" {
   }
 
 }
+
+
+resource "aws_subnet" "git-sub-2" {
+  cidr_block = "192.168.1.0/24"
+  vpc_id     = aws_vpc.name.id
+  tags = {
+    "Name" = "git-sub-2"
+  }
+
+}
