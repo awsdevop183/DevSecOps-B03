f5574842b8127c436ba278e34270b5d7b1ce4359
Madhukar Reddy <info@madhukarreddy>
2026-07-22 08:27:37 +0530

    Add route table

--- Git/network.tf
+++ Git/network.tf
@@ -2,11 +2,25 @@ resource "aws_vpc" "name" {
   # Arguments
   cidr_block = "192.168.0.0/16"
   tags = {
-    "Name": "Terraform-git-VPC"
+    "Name" : "Terraform-git-VPC"
   }
 }
 
 resource "aws_internet_gateway" "git-vpc-igw" {
   vpc_id = aws_vpc.name.id
-  
+
+}
+
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.name.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.git-vpc-igw.id
+  }
+
+  tags = {
+    Name = "Git-public-RT"
+  }
 }
