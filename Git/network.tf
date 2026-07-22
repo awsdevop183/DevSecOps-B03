175c9500e75d33c257a5c238e4cc4cf67955cb0f
Madhukar Reddy <info@madhukarreddy>
2026-07-22 08:48:13 +0530

    Change VPC cidr to 192.168.3.0/16

--- Git/network.tf
+++ Git/network.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "name" {
   # Arguments
-  cidr_block = "192.168.0.0/16"
+  cidr_block = "192.168.3.0/16"
   tags = {
     "Name" : "Terraform-git-VPC"
   }
--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "git-sub-1" {
-  cidr_block = "192.168.0.0/24"
+  cidr_block = "192.168.3.0/24"
   vpc_id     = aws_vpc.name.id
   tags = {
     "Name" = "git-sub-1"
