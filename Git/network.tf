bf30ef1c36f45b1a347376a10cae14b66384ffde
Madhukar Reddy <info@madhukarreddy>
2026-07-24 08:53:11 +0530

    fix network.tf

--- Git/network.tf
+++ Git/network.tf
@@ -6,22 +6,3 @@ resource "aws_vpc" "name" {
     "Name" = "Terraform-git-VPC"
   }
 }
-
-resource "aws_internet_gateway" "git-vpc-igw" {
-  vpc_id = aws_vpc.name.id
-
-}
-
-
-resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.name.id
-
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.git-vpc-igw.id
-  }
-
-  tags = {
-    Name = "Git-public-RT"
-  }
-}
