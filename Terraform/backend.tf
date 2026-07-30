2d0a01e428e1ef8f531e4a0f73ec79777f49c548
Madhukar Reddy <madhu@madhukarreddy.com>
2026-07-30 08:52:19 +0530

    Add dynamodb table and state lock with DynamoDB

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -6,3 +6,16 @@ terraform {
     use_lockfile = true
   }
 }
+
+
+# Old way of state locking using DynamoDB
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-bucket-26oct25"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "tf-state-lock-table"
+#     encrypt        = true
+#   }
+# }
