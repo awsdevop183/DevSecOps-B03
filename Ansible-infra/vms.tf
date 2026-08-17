4e77a0be41ec9d83883f5793e908ba03cf00f008
Madhukar Reddy <madhu@madhukarreddy.com>
2026-08-17 07:27:51 +0530

    Comment amazon-linux vm

--- Ansible-infra/vms.tf
+++ Ansible-infra/vms.tf
@@ -74,17 +74,17 @@ resource "aws_instance" "node" {
 
 }
 
-resource "aws_instance" "amazon-linux" {
-  ami                         = var.amzon-linux-ami
-  instance_type               = var.node_instance_type
-  key_name                    = var.key_name
-  subnet_id                   = aws_subnet.public-subnets["subnet-1"].id
-  vpc_security_group_ids      = [aws_security_group.node-sg.id]
-  associate_public_ip_address = true
+# resource "aws_instance" "amazon-linux" {
+#   ami                         = var.amzon-linux-ami
+#   instance_type               = var.node_instance_type
+#   key_name                    = var.key_name
+#   subnet_id                   = aws_subnet.public-subnets["subnet-1"].id
+#   vpc_security_group_ids      = [aws_security_group.node-sg.id]
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "centos"
-    Role = "node"
-  }
+#   tags = {
+#     Name = "centos"
+#     Role = "node"
+#   }
 
-}
+# }
